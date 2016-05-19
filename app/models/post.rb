bb8ba1e77e8d345091c8c13588ac77e0805ce243
saich <chsai.btech@gmail.com>
2016-05-19 02:34:24 +0000

    validation added to post

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,9 @@ class Post < ActiveRecord::Base
     # Active Record connect the class 'Post' and database table 'posts'
     # posts table contains - title, body
     
+    validates :title, :body, presence: true
+    
+    validates :title, length: { minimum: 8 }
+    
+    
 end
