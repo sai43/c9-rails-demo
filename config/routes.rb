7c2f1f5362f14bc03fb7b2d09ab4b858abd15827
saich <chsai.btech@gmail.com>
2016-05-19 03:06:05 +0000

    remove unwanted routes,countroller, model

--- config/routes.rb
+++ config/routes.rb
@@ -1,33 +1,7 @@
 Rails.application.routes.draw do
   
-  resources :posts
+   resources :posts
   
-  # get 'users/index'
-
-  # get 'users/new'
-
-  # post 'users/create'
-
-  # get 'users/show'
-
-  # get 'users/edit'
-
-  # put 'users/update'
-
-  # delete 'users/destroy'
-
-  # get 'welcome/index'
-
-  # get 'welcome/new'
-
-  # get 'welcome/create'
-
-  # get 'welcome/update'
-
-  # get 'welcome/destroy'
-
-  # get 'home/index'
-  
-  # get 'home/new'
    root 'posts#index'
+   
 end
